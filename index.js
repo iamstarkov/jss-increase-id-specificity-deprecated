39d11d3fc1d7f121002e4d00ed9026e366ac5516
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-06 18:21:26 +0100

    better hash

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => ({
   onProcessSheet: (sheet) => {
     const component = sheet && sheet.options.meta;
     if (!sheet.id) {
-      sheet.id = component + '-' + createHash(component);
+      sheet.id = component + '-' + createHash(sheet.toString());
     }
-    console.log(sheet);
 
-    console.log(sheet.rules.index);
     sheet.rules.index.forEach(rule => {
       rule.selector = '#' + sheet.id + rule.selector;
     })
