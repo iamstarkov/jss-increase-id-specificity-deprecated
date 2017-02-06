63e2caf86d8b3016ab290e93151260464ed7af8f
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-06 09:37:55 +0100

    wip++

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const createHash = require('murmurhash-js/murmurhash3_gc');
 
-const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => (rule, sheet) => {
-  const component = sheet && sheet.options.meta;
+const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => ({
+  onProcessSheet: (sheet) => {
+    const component = sheet && sheet.options.meta;
+    if (!sheet.id) {
+      sheet.id = component + '-' + createHash(component);
+    }
+    console.log(sheet.id)
 
-  if (!sheet.id) {
-    sheet.id = component + '-' + createHash(component);
+    Object.keys(sheet.classes).forEach(k => {
+      const v = sheet.classes[k];
+      // sheet.classes[k] = sheet.id.repeat(repeat) + v;
+    })
+
+    console.log(sheet);
   }
-
-  rule.selector = `#${sheet.id}`.repeat(repeat) + rule.selector;
-}
+})
 
 module.exports = increaseIdSpecificity;
