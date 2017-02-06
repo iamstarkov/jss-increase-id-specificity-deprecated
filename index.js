12093a5fbdc846c73d876c9b8a205a63a46ae5c1
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-06 20:58:46 +0100

    done?

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const createHash = require('murmurhash-js/murmurhash3_gc');
 const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => ({
   onProcessSheet: (sheet) => {
     const component = sheet && sheet.options.meta;
+
     if (!sheet.id) {
       sheet.id = component + '-' + createHash(sheet.toString());
     }
 
     sheet.rules.index.forEach(rule => {
-      rule.selector = '#' + sheet.id + rule.selector;
+      console.log({ rule });
+      console.log(typeof rule.originalStyle.rootNode)
+
+      if (typeof rule.originalStyle.rootNode !== 'boolean') {
+        return;
+      }
+
+      rule.selector = [
+        '#',
+        sheet.id,
+        (rule.originalStyle.rootNode ? '' : ' '),
+        rule.selector
+      ].join('');
+
+      rule.originalStyle.rootNode = null;
+      rule.style['root-node'] = null;
     })
   }
 });
