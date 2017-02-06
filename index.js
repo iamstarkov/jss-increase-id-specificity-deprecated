6d8d517d002e9b849fbb4943f3cffb5d17a5bc01
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-06 23:43:33 +0100

    wip

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const createHash = require('murmurhash-js/murmurhash3_gc');
 
 const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => ({
-  onProcessSheet: (sheet) => {
+  onProcessSheet(sheet) {
     const component = sheet && sheet.options.meta;
 
     if (!sheet.id) {
       sheet.id = component + '-' + createHash(sheet.toString());
     }
 
-    sheet.rules.index.forEach(rule => {
-      console.log({ rule });
-      console.log(typeof rule.originalStyle.rootNode)
+    const rootNode = rule => rule.originalStyle.rootNode;
+    const hasRootNode = rule => typeof rule.originalStyle.rootNode === 'boolean';
+    const parent = rule => !!rule.options.nestingLevel && rule.options.parent
+      ? rule.options.parent
+      : null;
+    const parents = (rule, acc=[]) => parent(rule)
+      ? parents(
+          parent(rule),
+          acc.concat( parent(rule) )
+        )
+      : acc;
 
+    // const parentRule =
+
+    const prefix = ('#' + sheet.id).repeat(repeat);
+    const glue = rule => hasRootNode(rule)
+      ? rootNode(rule) ? '' : ' '
+      : null;
+
+    const cleanup = rule => {
+      rule.originalStyle.rootNode = null;
+      rule.style['root-node'] = null;
+    }
+
+    sheet.rules.index.forEach(rule => {
+      console.log({
+        rule,
+        hasRootNode: hasRootNode(rule),
+        parents: parents(rule),
+        // parentsRootNode: parents(rule).map(rootNode),
+        glue: glue(rule)
+      });
       if (typeof rule.originalStyle.rootNode !== 'boolean') {
         return;
       }
 
-      rule.selector = [
-        '#',
-        sheet.id,
-        (rule.originalStyle.rootNode ? '' : ' '),
-        rule.selector
-      ].join('');
-
-      rule.originalStyle.rootNode = null;
-      rule.style['root-node'] = null;
+      rule.selector = prefix + glue(rule) + rule.selector;
     })
   }
 });
