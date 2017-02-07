0d6f3980ba424ecb0f0bba377ffe67716d61631e
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-07 13:07:46 +0100

    done with this

--- index.js
+++ index.js
@@ -9,43 +9,22 @@ const increaseIdSpecificity = ({ repeat }={ repeat: 3 }) => ({
     }
 
     const rootNode = rule => rule.originalStyle.rootNode;
-    const hasRootNode = rule => typeof rule.originalStyle.rootNode === 'boolean';
-    const parent = rule => !!rule.options.nestingLevel && rule.options.parent
-      ? rule.options.parent
-      : null;
-    const parents = (rule, acc=[]) => parent(rule)
-      ? parents(
-          parent(rule),
-          acc.concat( parent(rule) )
-        )
-      : acc;
-
-    // const parentRule =
+    const hasRootNode = rule => typeof rootNode(rule) === 'boolean';
 
     const prefix = ('#' + sheet.id).repeat(repeat);
-    const glue = rule => hasRootNode(rule)
-      ? rootNode(rule) ? '' : ' '
-      : null;
+    const glue = rule => rootNode(rule) ? '' : ' '
 
     const cleanup = rule => {
       rule.originalStyle.rootNode = null;
       rule.style['root-node'] = null;
     }
 
-    sheet.rules.index.forEach(rule => {
-      console.log({
-        rule,
-        hasRootNode: hasRootNode(rule),
-        parents: parents(rule),
-        // parentsRootNode: parents(rule).map(rootNode),
-        glue: glue(rule)
+    sheet.rules.index
+      .filter(hasRootNode)
+      .forEach(rule => {
+        rule.selector = prefix + glue(rule) + rule.selector;
+        cleanup(rule);
       });
-      if (typeof rule.originalStyle.rootNode !== 'boolean') {
-        return;
-      }
-
-      rule.selector = prefix + glue(rule) + rule.selector;
-    })
   }
 });
 
