b8fedf3daae71fe595db1624fd3fa87225eb6331
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-14 16:42:46 +0100

    move specificity fix into start of the selector

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ module.exports = function increaseIdSpecificity({ repeat }={ repeat: 3 }) {
   return {
     onProcessSheet(sheet) {
       sheet.rules.index.forEach(rule => {
-        const [ selector, pseudoelem ] = ("" + rule.selectorText).split('::');
-        rule.selectorText = selector;
-        rule.selectorText += ':not(#\\20)'.repeat(repeat);
-        if (pseudoelem) {
-          rule.selectorText += '::' + pseudoelem;
-        }
+        rule.selectorText = ':not(#\\20)'.repeat(repeat) + rule.selectorText;
       })
     }
   }
